c4820c51969620bafeb7500ffafa0abf8a7f5523
uppueswarnagamasthan-byte <uppueswarnagamasthan@gmail.com>
2026-03-26 11:06:36 -0400

    Create ClaimRepository.java

---
+++ src/main/java/com/eswar/claims/entity/src/main/java/com/eswar/claims/repository/ClaimRepository.java
@@ -0,0 +1,9 @@
+package com.eswar.claims.repository;
+
+import com.eswar.claims.entity.Claim;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClaimRepository extends JpaRepository<Claim, Long> {
+}
