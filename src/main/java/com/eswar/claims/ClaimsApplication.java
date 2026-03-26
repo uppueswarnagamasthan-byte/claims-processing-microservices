4c375f73c3ff58e042fea97cc576977ebd64f109
uppueswarnagamasthan-byte <uppueswarnagamasthan@gmail.com>
2026-03-26 10:55:41 -0400

    Create ClaimsApplication.java

---
+++ src/main/java/com/eswar/claims/ClaimsApplication.java
@@ -0,0 +1,11 @@
+package com.eswar.claims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClaimsApplication.class, args);
+    }
+}
